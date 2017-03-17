b09180410a0dfe98f0999295f6c1eed14e07b073
Anders Claesson <akc@akc.is>
2017-03-17 18:23:57 +0000

    Experimentally confirm the set partition conjecture

--- examples/SetParBij.hs
+++ examples/SetParBij.hs
@@ -5,8 +5,11 @@ import Data.Maybe
 type Par = [[Int]]
 
 -- This is the derived / natural bijection
-bij :: Par -> Maybe Par
-bij par = let n = sum (map length par) in lookup par (pairing !! n)
+bij :: Par -> Par
+bij par = fromMaybe err (lookup par (pairing !! n))
+  where
+    n = sum (map length par)
+    err = error "Not in the domain of the bijection"
 
 remove :: Eq a => a -> [[a]] -> Maybe (Int, [[a]])
 remove x = remove' 0
@@ -17,19 +20,32 @@ remove x = remove' 0
         | otherwise   = (\(i', yss) -> (i', xs : yss)) <$> remove' (i+1) xss
 
 -- Remove the largest element from a set partition. Return the resulting
--- set partition and the index of the block from which the maximum was
--- removed, counting from the right/end of the list.
-rmMax :: Par -> (Int, Par)
-rmMax par = (i, reverse par')
+-- set partition, the maximum, and the index of the block from which the
+-- maximum was removed, counting from the right/end of the list.
+removeMax :: Par -> (Int, Int, Par)
+removeMax par = (m, i, reverse par')
   where
     m = maximum $ map maximum par
     Just (i, par') = remove m (reverse par)
 
+-- Insert a new maximum (m+1) at position i counting from the right and
+-- disregarding the block that m belongs to.
+insertMax :: Int -> Int -> Par -> Par
+insertMax m i par = reverse $ ins i (reverse par)
+  where
+    ins _ [] = error "Oops, something went wrong"
+    ins i (xs : xss)
+        = if m `elem` xs
+          then xs : ins i xss
+          else if i == 0
+               then (xs ++ [m+1]) : xss
+               else xs : ins (i-1) xss
+
 -- Direct implementation of bij
--- TODO: Incomplete
-f :: Par -> (Par, Par, Int)
-f par =
-    let (i, par') = rmMax par
-    in case bij (filter (not . null) par') of
-         Nothing    -> error "Not in the domain of the bijection"
-         Just par'' -> (par, par'', i)
+direct :: Par -> Par
+direct []  = [[1]]
+direct par =
+    let (m, i, p) = removeMax par
+    in case break null p of
+         (q, [])   -> insertMax m i (bij q)
+         (q, [[]]) -> bij q ++ [[m+1]]
